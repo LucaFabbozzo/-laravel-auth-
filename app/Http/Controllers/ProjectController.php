b711a5b16277b4aaa654282e3bce71831feeb7a3
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-01-18 10:57:50 -0500

    print projects into a table

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -14,7 +14,7 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        $projects = Project::all();
+        $projects = Project::orderBy('id', 'desc')->paginate(5);
 
         return view('admin.projects.index', compact('projects'));
     }
--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,10 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container w-50 m-auto text-center pt-5 text-uppercase">
-        <h1>List</h1>
-        @dump($projects)
+    <div class="container w-75 m-auto text-center pt-5 text-uppercase">
+        <h1 class="fs-4 pb-4">My Projects List</h1>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Client Name</th>
+                    <th scope="col">Summary</th>
+                    <th scope="col">Cover</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($projects as $project)
+                <tr>
+                    <td>{{ $project->name }}</th>
+                    <td>{{ $project->client_name }}</td>
+                    <td>{{ $project->summary }}</td>
+                    <td><img style="width: 60px" src="{{ $project->cover_image }}" alt="{{ $project->name }}"></td>
+                </tr>
+                @empty
+
+                @endforelse
+            </tbody>
+        </table>
     </div>
+
+     {{$projects->links()}}
 @endsection
 
 @section('title')
